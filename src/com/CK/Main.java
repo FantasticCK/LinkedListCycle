0eeee63ec7271a81985062bf0cd86c282de3a36b
CK <kainingc@usc.edu>
2019-08-15 20:55:53 -0700

    first commit

---
+++ src/com/CK/Main.java
@@ -0,0 +1,26 @@
+package com.CK;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+class Solution{
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
